4fc9db3c4cf70e143845e56e82ede4478788308e
lucaono13 <gionorato@gmail.com>
2022-11-19 17:18:52 -0800

    Fixed Add Save Dialog disabling

--- frontend/src/components/TrackerFeatures.vue
+++ frontend/src/components/TrackerFeatures.vue
@@ -21,26 +21,26 @@
             <form method="POST" id='addForm' @submit.prevent="newSaveAdded(!v$.$invalid)">
                 <div class="field">
                     <div class="p-float-label mt-4 field">
-                        <InputText class="w-full" id="saveName" type="text" :disabled="submitted" v-model="v$.saveName.$model" autofocus :class="{'p-invalid':v$.saveName.$invalid && submitted}"/>
+                        <InputText class="w-full" id="saveName" type="text" :disabled="addingToDB" v-model="v$.saveName.$model" autofocus :class="{'p-invalid':v$.saveName.$invalid && submitted}"/>
                         <label for="saveName">Save Name*</label>
                         <small v-if="(v$.saveName.$invalid && submitted) || v$.saveName.$pending.$response" class="p-error">{{v$.saveName.required.$message.replace('Value', 'Save Name')}}</small>
                     </div>
                 </div>
                 <div class="p-float-label mt-4 field">
-                    <InputText class="w-full"  id="managerName" type="text" :disabled="submitted" v-model="v$.managerName.$model" :class="{'p-invalid':v$.managerName.$invalid && submitted}" />
+                    <InputText class="w-full"  id="managerName" type="text" :disabled="addingToDB" v-model="v$.managerName.$model" :class="{'p-invalid':v$.managerName.$invalid && submitted}" />
                     <label for="managerName">Manager Name*</label>
                     <small v-if="(v$.managerName.$invalid && submitted) || v$.managerName.$pending.$response" class="p-error">{{v$.managerName.required.$message.replace('Value', 'Manager Name')}}</small>
                 </div>
                 <div class="field mt-4" >
                     <!-- <label for="version">Game Version</label> -->
-                    <Dropdown class="w-full" v-model="v$.gameVersion.$model" id="gVersion" :disabled="submitted" :options="versions" optionLabel="name" optionValue="name" placeholder="Select Game Version" :class="{'p-invalid':v$.gameVersion.$invalid && submitted}"/>
+                    <Dropdown class="w-full" v-model="v$.gameVersion.$model" id="gVersion" :disabled="addingToDB" :options="versions" optionLabel="name" optionValue="name" placeholder="Select Game Version" :class="{'p-invalid':v$.gameVersion.$invalid && submitted}"/>
                     <small v-if="(v$.gameVersion.$invalid && submitted) || v$.gameVersion.$pending.$response" class="p-error">{{v$.gameVersion.required.$message.replace('Value', 'Game Version')}}</small>
                 </div>
             </form>
             <template #footer >
                 <div class="flex align-content-center justify-content-center">
-                    <Button label="Cancel" id="cancelB" :disabled="submitted" class='p-button-text' @click="closeAddSave" />
-                    <Button label="Add" id="submitB" :disabled="submitted" type="submit" form="addForm"/>
+                    <Button label="Cancel" id="cancelB" :disabled="addingToDB" class='p-button-text' @click="closeAddSave" />
+                    <Button label="Add" id="submitB" :disabled="addingToDB" type="submit" form="addForm"/>
                 </div>
                 <div v-if="addingToDB" class="mt-3 justify-content-start">
                     <p class="mb-1">Adding...</p>
