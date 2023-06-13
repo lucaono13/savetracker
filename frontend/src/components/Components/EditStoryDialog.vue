93d8ffc57fe7f0646994c0cee8816b3ee867e388
lucaono13 <gionorato@gmail.com>
2023-06-13 16:01:58 -0700

    Added EditStoryDialog component

---
+++ frontend/src/components/Components/EditStoryDialog.vue
@@ -0,0 +1,29 @@
+<template>
+    <Dialog modal :draggable="false" class="w-9" :close-on-escape="false" :closable="true" header="Edit your save story">
+        <Editor v-model="story"/>
+        <template #footer>
+            <Button label="Cancel" text @click="$emit('closeDialog')"/>
+            <Button label="Save" @click="updateStory"/>
+        </template>
+    </Dialog>
+</template>
+
+<script setup lang="ts">
+import { ref, defineProps, onMounted, defineEmits } from 'vue';
+import { backend } from '../../../wailsjs/go/models'
+
+const props = defineProps<{
+    story: string,
+}>()
+const emit = defineEmits(['closeDialog','updateStory'])
+const story = ref('')
+onMounted( () => {
+    story.value = props.story
+})
+
+function updateStory() {
+    emit('updateStory', story.value)
+    emit('closeDialog')
+}
+
+</script>
